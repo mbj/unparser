946297ac7e5cfaf9b75290244aff5e17d2152fc7
Markus Schirp <mbj@schirp-dso.com>
2013-04-08 13:32:05 +0200

    Initial spike

--- lib/unparser.rb
+++ lib/unparser.rb
@@ -1,3 +1,102 @@
+require 'abstract_type'
+require 'concord'
+require 'parser/all'
+
 # Library namespace
 module Unparser
+
+  class Buffer
+    def initialize
+      @content = ''
+    end
+
+    def append(string)
+      @content << string
+    end
+
+    def content
+      @content.dup.freeze
+    end
+  end
+
+  class Emitter
+    include AbstractType
+
+    def initialize(node, buffer)
+      @node, @buffer = node, buffer
+      dispatch
+    end
+
+  private
+
+    abstract_method :dispatch
+
+    attr_reader :node
+    private :node
+    attr_reader :buffer
+    private :buffer
+
+    def emit(string)
+      buffer.append(string)
+    end
+
+    def self.visit(node, buffer)
+      type = node.type
+      handler = REGISTRY.fetch(type) do 
+        raise ArgumentError, "No emitter for node: #{type.inspect}"
+      end
+
+      handler.new(node, buffer)
+    end
+
+    REGISTRY = {}
+
+    def self.handle(type)
+      REGISTRY[type] = self
+    end
+
+    class Literal < self
+      def value
+        node.children[0]
+      end
+
+      class Inspect < self
+        handle :str
+        handle :int
+
+      private
+
+        def dispatch
+          emit(value.inspect)
+        end
+      end
+
+      class Dynamic < self
+        class String < self
+          handle :dstr
+
+        private
+
+          def dispatch
+            p node
+          end
+        end
+      end
+    end
+  end
+
+  # Unparse ast into string
+  #
+  # @param [Parser::Node] node
+  #
+  # @return [String]
+  #
+  # @api private
+  #
+  def self.unparse(ast)
+    buffer = Buffer.new
+    Emitter.visit(ast, buffer)
+    buffer.content
+  end
+
 end
---
+++ spec/integration/unparser/spike_spec.rb
@@ -0,0 +1,60 @@
+require 'spec_helper'
+
+describe Unparser, 'spike' do
+
+  let(:diagnostics) { [] }
+
+  def parser_for_ruby_version(version)
+    case version
+    when '1.8'; parser = Parser::Ruby18.new
+    # when '1.9'; parser = Parser::Ruby19.new # not yet
+    # when '2.0'; parser = Parser::Ruby20.new # not yet
+    else raise "Unrecognized Ruby version #{version}"
+    end
+
+    parser.diagnostics.consumer = lambda do |diagnostic|
+      diagnostics << diagnostic
+    end
+
+    parser
+  end
+
+  def with_versions(code, versions)
+    versions.each do |version|
+      diagnostics.clear
+
+      parser = parser_for_ruby_version(version)
+      yield version, parser
+    end
+  end
+
+  def assert_round_trip(input, versions = %w(1.8))
+    with_versions(input, versions) do |version, parser|
+      source = Parser::Source::Buffer.new('(unparser')
+      source.source = input
+      ast = parser.parse(source)
+      Unparser.unparse(ast).should eql(input)
+    end
+  end
+
+  def self.assert_round_trip(input, versions = %w(1.8))
+    it "should round trip #{input.inspect}" do
+      assert_round_trip(input, versions)
+    end
+  end
+
+  context 'literal' do
+    context 'fixnum' do
+      assert_round_trip  '1'
+      assert_round_trip '-1'
+      assert_round_trip  '0'
+    end
+
+    context 'string' do
+      assert_round_trip %q("foo")
+      assert_round_trip %q("foo" "bar")
+      assert_round_trip %q("foo#{1}bar")
+      assert_round_trip %q("foo\n")
+    end
+  end
+end
