de311166ef476c15851c94ff6b920bb9162a5326
Markus Schirp <mbj@schirp-dso.com>
2013-05-11 23:51:41 +0200

    Kill mutants in Emitter.handler

---
+++ spec/unit/unparser/emitter/class_methods/handle_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe Unparser::Emitter, '.handle' do
+  subject { class_under_test.class_eval { handle :foo } }
+
+  let(:class_under_test) do
+    Class.new(described_class)
+  end
+
+  before do
+    stub_const('Unparser::Emitter::REGISTRY', {})
+  end
+
+  it 'should register emitter' do
+    expect { subject }.to change { Unparser::Emitter::REGISTRY }.from({}).to(:foo => class_under_test)
+  end
+end
