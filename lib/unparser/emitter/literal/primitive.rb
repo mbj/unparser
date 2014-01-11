54d0415c437a24385c30aaa542c11c10375e889e
Markus Schirp <mbj@schirp-dso.com>
2014-01-11 18:51:03 +0100

    Remove unused code for __FILE__ and __LINE__ macros

--- lib/unparser/emitter/literal/primitive.rb
+++ lib/unparser/emitter/literal/primitive.rb
@@ -49,65 +49,6 @@ module Unparser
           end
 
         end # Numeric
-
-        # Literal emitter with macro regeneration base class
-        class MacroSafe < self
-
-        private
-
-          # Perform dispatch
-          #
-          # @return [undefined]
-          #
-          # @api private
-          #
-          def dispatch
-            if source == macro
-              write(macro)
-              return
-            end
-            write(value.inspect)
-          end
-
-          # Return source, if present
-          #
-          # @return [String]
-          #   if present
-          #
-          # @return [nil]
-          #   otherwise
-          #
-          # @api private
-          #
-          def source
-            location = node.location || return
-            expression = location.expression || return
-            expression.source
-          end
-
-          # Return marco
-          #
-          # @return [String]
-          #
-          # @api private
-          #
-          def macro
-            self.class::MACRO
-          end
-
-         ## String macro safe emitter
-         #class String < self
-         #  MACRO = '__FILE__'.freeze
-         #  handle :str
-         #end # String
-
-         ## Integer macro safe emitter
-         #class Integer < self
-         #  MACRO = '__LINE__'.freeze
-         #  handle :int
-         #end # Integer
-
-        end # MacroSave
       end # Primitive
     end # Literal
   end # Emitter
