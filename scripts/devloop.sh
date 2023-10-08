c996bbaee12b1b8c873b9e4408eaf339bfe5f799
Markus Schirp <mbj@schirp-dso.com>
2023-10-08 02:07:02 +0000

    Change to reduced devloop

--- scripts/devloop.sh
+++ scripts/devloop.sh
@@ -1,4 +1,4 @@
-while inotifywait ../mutant/**/*.rb **/*.rb Gemfile unparser.gemspec; do
+while inotifywait lib/**/*.rb test/**/*.rb spec/**/*.rb Gemfile unparser.gemspec; do
   bundle exec rspec spec/unit -fd --fail-fast --order default \
     && bundle exec ./bin/parser-round-trip-test \
     && bundle exec mutant run --zombie --since HEAD~1 --fail-fast -- 'Unparser*' \
